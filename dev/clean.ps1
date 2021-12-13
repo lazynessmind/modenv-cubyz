10a93d17c69db91649235d292d83df1b3fd0cccd
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-12-13 19:37:13 +0000

    Add run that clean the workspace

---
+++ dev/clean.ps1
@@ -0,0 +1,41 @@
+Write-Host "Cleaning workspace..." -ForegroundColor black -BackgroundColor White
+
+$assetsCubyz = Test-Path "./assets/cubyz/"
+$assetsBackground = Test-Path "./assets/backgrounds/"
+$mods = Test-Path "./mods/"
+$logs = Test-Path "./logs/"
+$settings = Test-Path "./settings.json"
+
+# assets
+if ($assetsCubyz)
+{
+    if ($assetsBackground)
+    {
+        Write-Host "Removing cubyz assets..." -ForegroundColor black -BackgroundColor White
+        Remove-Item -Recurse -Force "./assets/cubyz/"
+        Remove-Item -Recurse -Force "./assets/backgrounds/"
+    }
+}
+# mods
+if ($mods)
+{
+    Write-Host "Removing mods folder..." -ForegroundColor black -BackgroundColor White
+    Remove-Item -Recurse -Force "./mods/"
+}
+
+# logs
+if ($logs)
+{
+    Write-Host "Removing mods folder..." -ForegroundColor black -BackgroundColor White
+    Remove-Item -Recurse -Force "./logs/"
+}
+
+# settings
+if ($settings)
+{
+    Write-Host "Removing settings.json..." -ForegroundColor black -BackgroundColor White
+    Remove-Item -Recurse -Force "./settings.json"
+}
+
+Write-Host "Done." -ForegroundColor black -BackgroundColor White
+Write-Host "Note: Don't forget to rerun setup.ps1 again." -ForegroundColor White -BackgroundColor Yellow
